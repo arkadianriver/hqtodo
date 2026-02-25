b047ca6b104c0132621075cdbdfda2604d3191d1
Gary Faircloth <arkadianriver@gmail.com>
2026-02-25 15:42:18 -0600

    set vite base for gh-pages

--- client-react-carbon/vite.config.js
+++ client-react-carbon/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: import.meta.env.VITE_DEMO === true ? '/hqtodo/' : '/',
   plugins: [react()],
   server: {
     port: '3001',
