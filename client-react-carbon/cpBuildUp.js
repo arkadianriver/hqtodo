c621f0813baf4e25cc9a3fd5b8cfecca129240ac
Gary Faircloth <arkadianriver@gmail.com>
2026-02-25 17:40:34 -0600

    pass base from build cmd

--- client-react-carbon/cpBuildUp.js
+++ client-react-carbon/cpBuildUp.js
@@ -3,7 +3,10 @@ const fs = require("fs");
 const src = "./build/";
 const dest = "../build/";
 
-console.log(`Copying ${src} to ${dest}...`);
+console.log(`Cleaning destination folder: ${dest}.`);
+fs.rmSync(dest, {recursive: true, force: true});
+
+console.log(`Copying ${src} to ${dest}.`);
 fs.cpSync(src, dest, {recursive: true});
 fs.writeFileSync(`${dest}.nojekyll`, "");
 
--- client-react-carbon/vite.config.js
+++ client-react-carbon/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.VITE_DEMO === true ? '/hqtodo/' : '/',
   plugins: [react()],
   server: {
     port: '3001',
