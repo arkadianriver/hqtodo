d91a255d43924794dbb7da8bde547fd5d498cefe
Gary Faircloth <arkadianriver@gmail.com>
2026-02-25 19:30:16 -0600

    bump version .. .. and remove unused dependencies

--- client-react-carbon/src/main.jsx
+++ client-react-carbon/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router";
 
 const basename = import.meta.env.VITE_DEMO !== "true" ? '/' : '/hqtodo'
@@ -13,8 +12,3 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
